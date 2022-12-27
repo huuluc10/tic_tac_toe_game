609e52d9a59088433163cda22f788dd85eb856b4
huuluc10 <lucnguyenhuu91@gmail.com>
2022-12-28 00:37:49 +0700

    Thay đổi cách đưa ra quyết định trong Monte - Carlo

---
+++ main.java
@@ -0,0 +1,9 @@
+class main {
+    public static void main(String[] args) {
+        float a;
+        for(int i = 1; i < Integer.MAX_VALUE; i++) {
+            System.out.println(Math.sqrt(Math.pow(i,2)/2));
+        }
+        
+    }
+}
